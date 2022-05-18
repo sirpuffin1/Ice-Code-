f869ab63a68c80a796fea1f32ed50325189b6d7f
Angel <angelcruz981@yahoo.com>
2022-05-18 11:53:22 -0700

    feat(main): use apiservice to get books then update html

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,13 @@
 import { apiService as api } from '../api/api.service.js';
+api.get('bestsellers').then(data => {
+    data.forEach((book, index) => {
+        console.log(index)
+        let bookImage = document.getElementById(`image${index}`)
+        let bookTitle = document.getElementById(`title${index}`)
+        let bookAuthor = document.getElementById(`author${index}`)
+        
+        bookImage.src=book.imgUrl
+        bookTitle.innerHTML = book.title
+        bookAuthor.innerHTML = book.author
+    })
+})
