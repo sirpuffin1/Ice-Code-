2fcfef49e633c37048e41e42e72f66608d244713
Isabella Tannady <93556334+itannady@users.noreply.github.com>
2022-05-23 10:54:42 -0700

    Revert "Kf 12 added table"

--- client/api/api.service.js
+++ client/api/api.service.js
@@ -1,8 +1,3 @@
 export const apiService = ({
-    baseUrl: 'http://localhost:3001/',
-    //create the option to use the apiservice to get information from routes
-    get(url) {
-        //fetching information from the baseroute + whatever url route we give it
-        return fetch(this.baseUrl + url).then(res => res.json())
-    }
+    baseUrl: 'http://localhost:3000/'
 })
