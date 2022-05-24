026058df469b609a5f6fa9d320c5d1f4870705f9
moon68work <czurowski@alphaworks.tech>
2022-05-24 10:17:36 -0700

    feat(add): Added Modals

--- client/js/main.js
+++ client/js/main.js
@@ -20,3 +20,42 @@ window.onload = refreshBooks;
 const button = document.getElementById('refresh-icon');
 
 button.addEventListener('click', refreshBooks);
+
+
+//Modals
+
+const noteModal = document.querySelector("#noteModal");
+const noteIcon = document.querySelector("#noteIcon");
+const closeNoteModal = document.querySelector("#closeNoteModal");
+
+noteIcon.onclick = function() {
+  noteModal.style.display = "block";
+}
+
+closeNoteModal.onclick = function() {
+  noteModal.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target == noteModal) {
+        noteModal.style.display = "none";
+    }
+})
+
+const newBookModal = document.querySelector("#newBookModal");
+const newBookButton = document.querySelector("#newBookBtn");
+const closeNewBookModal = document.querySelector("#closeNewBookModal");
+
+newBookButton.onclick = function() {
+  newBookModal.style.display = "block";
+}
+
+closeNewBookModal.onclick = function() {
+  newBookModal.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target == newBookModal) {
+    newBookModal.style.display = "none";
+    }
+})
